315a108f2d75b15bb8f97424cfba4ff49f587b0f
DontLikenaming <edirna21@gmail.com>
2023-05-23 18:11:53 +0900

    회원가입 시 회원 정보를 위한 모델 클래스 작성 (Transient 애노테이션 추가)

--- src/main/java/dontlikenaming/springboot/semiprojectv7/model/Member.java
+++ src/main/java/dontlikenaming/springboot/semiprojectv7/model/Member.java
@@ -5,15 +5,12 @@ import lombok.Setter;
 import lombok.ToString;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.data.annotation.CreatedDate;
 
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.Id;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Column;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 @Entity
@@ -37,14 +34,20 @@ public class Member {
 
     private String pnum3;
 
+    @NotBlank(message = "아이디는 필수 항목입니다!")
+    @Length(min = 6, max = 16, message = "아이디는 최소 6자, 최대 16자여야 합니다!")
     private String userid;
 
+    @NotBlank(message = "비밀번호를 입력해주세요!")
+    @Length(min = 6, max = 16, message = "비밀번호는 최소 6자, 최대 16자여야 합니다!")
     private String passwd;
 
     private String zipcode;
 
+    @NotBlank(message = "주소를 입력해주세요!")
     private String addr1;
 
+    @NotBlank(message = "주소를 입력해주세요!")
     private String addr2;
 
     private String email;
@@ -52,4 +55,22 @@ public class Member {
     @CreatedDate
     @Column(insertable = false, updatable = false)
     private LocalDateTime regdate;
+
+
+
+    @Transient  // 필드 영속성 지원 x
+    @NotBlank(message = "우편번호를 입력해주세요!")
+    private String zip1;
+
+    @Transient
+    @NotBlank(message = "우편번호를 입력해주세요!")
+    private String zip2;
+
+    @Transient
+    @NotBlank(message = "이메일을 입력해주세요!")
+    private String email1;
+
+    @Transient
+    @NotBlank(message = "이메일을 입력해주세요!")
+    private String email2;
 }
