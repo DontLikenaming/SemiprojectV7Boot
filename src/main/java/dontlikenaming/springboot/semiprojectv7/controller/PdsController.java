ddddd5d365d81644a3e27baf94be490df8c9b476
DontLikenaming <edirna21@gmail.com>
2023-05-24 18:27:25 +0900

    자료실 - 작성된 글 데이터 및 첨부 파일 데이터 저장 1 - PdsController 작성

--- src/main/java/dontlikenaming/springboot/semiprojectv7/controller/PdsController.java
+++ src/main/java/dontlikenaming/springboot/semiprojectv7/controller/PdsController.java
@@ -1,15 +1,53 @@
 package dontlikenaming.springboot.semiprojectv7.controller;
 
+import dontlikenaming.springboot.semiprojectv7.model.Pds;
+import dontlikenaming.springboot.semiprojectv7.service.PdsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import java.util.Map;
 
 @Controller
 @RequestMapping(value = "/pds")
 public class PdsController {
+    @Autowired private PdsService pdssrv;
 
     @GetMapping(value = "/list")
-    public String list(){
-        return "pds/list";
+    public ModelAndView list(Integer page){
+        if((page==null)||(page<=0)){page = 1;}
+        Map<String, Object> pds = pdssrv.readPds(page);
+
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("pds/list");
+        mv.addObject("pdslist", pds.get("pdslist"));
+        mv.addObject("page", page);
+        mv.addObject("stpg", (page-1)/10*10+1);
+        mv.addObject("cntpg", pds.get("cntpg"));
+
+        return mv;
+    }
+
+    @GetMapping(value = "/write")
+    public String write(Model m){
+        m.addAttribute("pds", new Pds());
+        return "pds/write";
+    }
+
+    @PostMapping("/write")
+    public String writeok(@Valid Pds pds, BindingResult br, MultipartFile attach){
+        String view = "pds/write";
+
+        int pno = pdssrv.newPds(pds);
+        if(pdssrv.newPdsAttach(attach, pno))view = "redirect:/pds/list";
+
+        return view;
     }
 }
