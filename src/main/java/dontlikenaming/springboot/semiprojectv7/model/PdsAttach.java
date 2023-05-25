aac78681441aed1d27516adc5e865553ecd69e6c
DontLikenaming <edirna21@gmail.com>
2023-05-25 11:39:52 +0900

    자료실 - PdsAttach 수정

--- src/main/java/dontlikenaming/springboot/semiprojectv7/model/PdsAttach.java
+++ src/main/java/dontlikenaming/springboot/semiprojectv7/model/PdsAttach.java
@@ -6,7 +6,13 @@ import lombok.ToString;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+
 
 @Entity
 @Table(name = "pdsattach")
