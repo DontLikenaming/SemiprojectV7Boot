e5be4ffa003611b36a4b7c7e247b8584dbacdf34
DontLikenaming <edirna21@gmail.com>
2023-05-26 16:09:23 +0900

    자료실 댓글 - 댓글쓰기 repository 구현

--- src/main/java/dontlikenaming/springboot/semiprojectv7/repository/PdsReplyRepository.java
+++ src/main/java/dontlikenaming/springboot/semiprojectv7/repository/PdsReplyRepository.java
@@ -1,9 +1,11 @@
 package dontlikenaming.springboot.semiprojectv7.repository;
 
 import dontlikenaming.springboot.semiprojectv7.model.PdsReply;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -11,4 +13,9 @@ public interface PdsReplyRepository extends PagingAndSortingRepository<PdsReply,
 
     //@Query("from PdsReply where pno = :pno order by refno, regdate asc")
     List<PdsReply> findByPnoOrderByRefnoAscRegdateAsc(Integer pno);
+
+    @Modifying
+    @Transactional
+    @Query("update PdsReply set refno = :rpno where rpno = :rpno")
+    void updateRefno(@Param("rpno") Integer rpno);
 }
